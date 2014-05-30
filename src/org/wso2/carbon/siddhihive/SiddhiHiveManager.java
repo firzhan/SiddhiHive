b780daf8bd280f16455f34f4dccc6e696960257e
tishan89 <dtishanpubudu@gmail.com>
2014-05-30 15:16:15 +0530

    adding SiddhiHiveManager and Constants classes

---
+++ src/org/wso2/carbon/siddhihive/SiddhiHiveManager.java
@@ -0,0 +1,8 @@
+package org.wso2.carbon.siddhihive;
+
+/*
+Class to manage query conversion in higher level. Will call appropriate handlers
+ */
+public class SiddhiHiveManager {
+
+}
---
+++ src/org/wso2/carbon/siddhihive/utils/Constants.java
@@ -0,0 +1,6 @@
+package org.wso2.carbon.siddhihive.utils;
+
+
+public final class Constants {
+    public final static String TIME_WINDOW = "time";
+}
