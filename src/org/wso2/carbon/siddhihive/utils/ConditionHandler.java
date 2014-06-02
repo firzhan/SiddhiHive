81668346501b488649de99042169bcc52e9570ed
firzhan <firzhan007@gmail.com>
2014-06-02 15:50:12 +0530

    adding conditional handler

---
+++ src/org/wso2/carbon/siddhihive/utils/ConditionHandler.java
@@ -0,0 +1,99 @@
+package org.wso2.carbon.siddhihive.utils;
+
+import org.wso2.siddhi.query.api.condition.*;
+import org.wso2.siddhi.query.api.expression.Expression;
+import org.wso2.siddhi.query.api.expression.Variable;
+import org.wso2.siddhi.query.api.expression.constant.Constant;
+import org.wso2.siddhi.query.api.expression.constant.IntConstant;
+
+/**
+ * Created by root on 6/2/14.
+ */
+public class ConditionHandler {
+
+    public ConditionHandler() {
+
+    }
+
+    public String processCondition(Condition condition) {
+
+        String handleCondition = " ";
+
+        if (condition == null)
+            return " ";
+
+        if (condition instanceof Compare) {
+            handleCondition += handleCompareCondition((Compare) condition);
+        } else if (condition instanceof AndCondition) {
+            //do
+        } else if (condition instanceof BooleanCondition) {
+            //do
+        } else if (condition instanceof InCondition) {
+            //do
+        } else if (condition instanceof NotCondition) {
+            //do
+        } else if (condition instanceof OrCondition) {
+            //do
+        }
+
+        return handleCondition;
+
+    }
+
+    private String handleCompareCondition(Compare compare) {
+
+        String leftExpressiveValue = handleCompareExpression(compare.getLeftExpression());
+        String rightExpressiveValue = handleCompareExpression(compare.getRightExpression());
+
+
+        String operatorString = getOperator(compare.getOperator());
+
+        return " " + leftExpressiveValue + "  " + operatorString + "  " + rightExpressiveValue;
+
+    }
+
+
+    private String handleCompareExpression(Expression expression) {
+
+        String expressionValue = " ";
+
+        if (expression instanceof Variable) {
+            expressionValue = handleVariable((Variable) expression);
+        } else if (expression instanceof Constant) {
+
+            if (expression instanceof IntConstant) {
+
+                IntConstant intConstant = (IntConstant) expression;
+                expressionValue = intConstant.getValue().toString();
+            }
+        }
+
+        return expressionValue;
+    }
+
+    private String handleVariable(Variable variable) {
+        return (variable.getStreamId() != null ? (variable.getStreamId() + ".") : "") + variable.getAttributeName();
+    }
+
+    private String getOperator(Condition.Operator operator) {
+
+
+        if (operator == Condition.Operator.EQUAL)
+            return " = ";
+        else if (operator == Condition.Operator.GREATER_THAN)
+            return " > ";
+        else if (operator == Condition.Operator.GREATER_THAN_EQUAL)
+            return " >= ";
+        else if (operator == Condition.Operator.LESS_THAN)
+            return " < ";
+        else if (operator == Condition.Operator.LESS_THAN_EQUAL)
+            return " <= ";
+        else if (operator == Condition.Operator.CONTAINS)
+            return " CONTAINS ";
+//        else if(operator == Condition.Operator.INSTANCE_OF)
+//            return " = ";
+
+
+        return " ";
+    }
+}
