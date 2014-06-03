6cfed4645c1f511e523ef5948fcd357191c6d374
tishan89 <dtishanpubudu@gmail.com>
2014-06-03 13:55:46 +0530

    API change

--- src/org/wso2/carbon/siddhihive/headerprocessor/StreamHandler.java
+++ src/org/wso2/carbon/siddhihive/headerprocessor/StreamHandler.java
@@ -4,7 +4,9 @@ package org.wso2.carbon.siddhihive.headerprocessor;
 import org.wso2.siddhi.query.api.definition.StreamDefinition;
 import org.wso2.siddhi.query.api.query.input.Stream;
 
+import java.util.Map;
+
 public interface StreamHandler {
 
-    public void process(Stream stream, StreamDefinition streamDefinition);
+    public Map<String, String> process(Stream stream, StreamDefinition streamDefinition);
 }
